7cee01ab44f5fc28624d5a596e5aa7d924c20b07
J-aidan <jacobsaidan53@gmail.com>
2024-11-23 22:05:10 -0500

    fixed hw4 messed up during hw5

---
+++ hw4/Scene.cpp
@@ -0,0 +1,11 @@
+/**
+* Author: Aidan Jacobs
+* Assignment: Rise of the AI
+* Date due: 2024-11-9, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#include "Scene.h"
